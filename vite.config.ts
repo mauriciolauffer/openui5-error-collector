637d147ac019950e9f98c8ace35de6f96ddb2b3c
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2023-03-15 17:43:59 +1100

    feat: Set last syncronization time

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    sourcemap: true,
     lib: {
       formats: ['iife'],
       entry: path.resolve(__dirname, 'src/ui5ErrorCollector.js'),
